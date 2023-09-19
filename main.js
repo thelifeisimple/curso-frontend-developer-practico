1a21eb45d24f89ea9b766591748b534c371941f8
thelifeisimple <anamariazuluagavasquez@gmail.com>
2023-09-18 23:03:31 -0300

    add mobile @mediaqueries

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
-const deskTopMenu = document.querySelector('.desktop-menu')
+const deskTopMenu = document.querySelector('.desktop-menu');
+const burgerMenuIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+burgerMenuIcon.addEventListener('click', toggleMobilepMenu);
 
 function toggleDesktopMenu() {
     console.log('Click');
     deskTopMenu.classList.toggle('inactive');
 }
 
-console.log('js fucionando');
+function toggleMobilepMenu() {
+    console.log('Click');
+    mobileMenu.classList.toggle('inactive');
+}
