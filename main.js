29c9c4450bd74b2f2d3aea52624ad2d5566f2dc5
thelifeisimple <anamariazuluagavasquez@gmail.com>
2023-09-18 23:59:43 -0300

    add aside and interactive inactive prop

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 const menuEmail = document.querySelector('.navbar-email');
-const deskTopMenu = document.querySelector('.desktop-menu');
 const burgerMenuIcon = document.querySelector('.menu');
+const menushoppingCartIcon = document.querySelector('.navbar-shopping-cart');
+const deskTopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 burgerMenuIcon.addEventListener('click', toggleMobilepMenu);
+menushoppingCartIcon.addEventListener('click', toggleCarritoAside);
 
 function toggleDesktopMenu() {
-    console.log('Click');
     deskTopMenu.classList.toggle('inactive');
 }
 
 function toggleMobilepMenu() {
-    console.log('Click');
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+    
 }
